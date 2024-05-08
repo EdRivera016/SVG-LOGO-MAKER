985aeec1b6b20d290a1cb0e96b7c8331e6939030
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-05-08 15:11:04 -0400

    download npms and also created proper file also started index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require("inquirer")
+const {Circle, Square, Triangle} = require("./lib/shapes");
+
+class Svg{
+    constructor(){}
+}
